d5c879ffe03d4c8278747d45eb9836a92caf64b6
juwon.kim <saylily90@naver.com>
2024-07-05 09:21:27 +0900

    driver added

--- Project12/DeviceDriver.cpp
+++ Project12/DeviceDriver.cpp
@@ -1,16 +1,67 @@
 #include "DeviceDriver.h"
+#include <iostream>
+#include <stdexcept>
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
+int DeviceDriver::CheckSameValue(long address)
+{
+    int value = (int)(m_hardware->read(address));
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (value == (int)(m_hardware->read(address))) continue;
+
+        throw out_of_range("value is different!!");
+    }
+
+    return value;
+}
+
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int value;
+    try
+    {
+        value = CheckSameValue(address);
+        return (int)(m_hardware->read(address));
+    }
+    catch (out_of_range& e)
+    {
+        cout << e.what() << endl;//<< " not same value\n";
+    }
+
+    return value;
+}
+
+bool DeviceDriver::CheckAbleToWrite(long address)
+{
+    if (0xFF == (int)(m_hardware->read(address)))
+    {
+        return true;
+    }
+
+    return false;
 }
 
 void DeviceDriver::write(long address, int data)
 {
     // TODO: implement this method
-    m_hardware->write(address, (unsigned char)data);
+    try
+    {
+        if (true == CheckAbleToWrite(address))
+        {
+            m_hardware->write(address, (unsigned char)data);
+            return;
+        }
+
+        throw invalid_argument("its value has already been written");
+    }
+    catch (invalid_argument& e)
+    {
+        cout << e.what() << endl;//<< " not same value\n";
+    }
 }
--- Project12/DeviceDriver.h
+++ Project12/DeviceDriver.h
@@ -7,6 +7,8 @@ public:
     DeviceDriver(FlashMemoryDevice* hardware);
     int read(long address);
     void write(long address, int data);
+    int CheckSameValue(long address);
+    bool CheckAbleToWrite(long address);
 
 protected:
     FlashMemoryDevice* m_hardware;
